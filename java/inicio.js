26a4ac39331ca6635991887174963cc4aa629c94
juan camilo <juancholm123@gmail.com>
2020-07-23 10:31:23 -0500

    cuarta version edwin

--- java/inicio.js
+++ java/inicio.js
@@ -2,12 +2,8 @@ function iniciar(){
 var usuario = document.getElementById('usuario');
 var clave = document.getElementById('clave');
 if(usuario.value=="juan" && clave.value=="123"){
-    
-    window.location.href="inicio.html";
-   alert("iniciando");
-} else{
-    alert("usuario no logueado");
-}
+    window.location.href="index.html";
+} 
 }
 
 
